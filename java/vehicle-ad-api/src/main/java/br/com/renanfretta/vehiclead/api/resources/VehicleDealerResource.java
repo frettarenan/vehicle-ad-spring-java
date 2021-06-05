3f8e33c021f1506cc4c7757644a62270ee7a4c24
frettarenan <frettarenan@gmail.com>
2021-06-05 18:03:35 -0300

    save implemented

--- java/vehicle-ad-api/src/main/java/br/com/renanfretta/vehiclead/api/resources/VehicleDealerResource.java
+++ java/vehicle-ad-api/src/main/java/br/com/renanfretta/vehiclead/api/resources/VehicleDealerResource.java
@@ -1,16 +1,18 @@
 package br.com.renanfretta.vehiclead.api.resources;
 
+import br.com.renanfretta.vehiclead.api.dtos.vehicledealer.input.VehicleDealerInputDTO;
 import br.com.renanfretta.vehiclead.api.dtos.vehicledealer.output.VehicleDealerOutputDTO;
 import br.com.renanfretta.vehiclead.api.exceptions.ResourceNotFoundException;
 import br.com.renanfretta.vehiclead.api.services.VehicleDealerService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,6 +22,7 @@ import java.util.List;
 public class VehicleDealerResource {
 
     private final VehicleDealerService service;
+    private final ObjectMapper objectMapper;
 
     @GetMapping
     public ResponseEntity<List<VehicleDealerOutputDTO>> findAll() {
@@ -43,6 +46,21 @@ public class VehicleDealerResource {
         } catch (ResourceNotFoundException e) {
             log.info("VehicleDealerResource/findAll notFound");
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<VehicleDealerOutputDTO> save(@Valid @RequestBody VehicleDealerInputDTO inputDTO) {
+        saveWasCalledLog(inputDTO);
+        VehicleDealerOutputDTO outputDTO = service.save(inputDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(outputDTO);
+    }
+
+    private void saveWasCalledLog(VehicleDealerInputDTO inputDTO) {
+        try {
+            log.info("VehicleDealerResource/save( " + objectMapper.writeValueAsString(inputDTO) + ") was called");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
     }
 
--- java/vehicle-ad-api/src/main/java/br/com/renanfretta/vehiclead/api/services/VehicleDealerService.java
+++ java/vehicle-ad-api/src/main/java/br/com/renanfretta/vehiclead/api/services/VehicleDealerService.java
@@ -2,10 +2,13 @@ package br.com.renanfretta.vehiclead.api.services;
 
 import br.com.renanfretta.vehiclead.api.commons.MessagesProperty;
 import br.com.renanfretta.vehiclead.api.configs.OrikaMapper;
+import br.com.renanfretta.vehiclead.api.dtos.vehicledealer.input.VehicleDealerInputDTO;
 import br.com.renanfretta.vehiclead.api.dtos.vehicledealer.output.VehicleDealerOutputDTO;
 import br.com.renanfretta.vehiclead.api.entities.VehicleDealer;
 import br.com.renanfretta.vehiclead.api.exceptions.ResourceNotFoundException;
 import br.com.renanfretta.vehiclead.api.repositories.VehicleDealerRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,6 +25,7 @@ public class VehicleDealerService {
     private final VehicleDealerRepository repository;
     private final OrikaMapper orikaMapper;
     private final MessagesProperty messagesProperty;
+    private final ObjectMapper objectMapper;
 
     public List<VehicleDealerOutputDTO> findAll() {
         List<VehicleDealer> list = repository.findAll();
@@ -37,6 +41,21 @@ public class VehicleDealerService {
         } catch (ResourceNotFoundException e) {
             log.warn("VehicleDealerRepository/findById(" + id + ") notFound");
             throw e;
+        }
+    }
+
+    public VehicleDealerOutputDTO save(VehicleDealerInputDTO inputDTO) {
+        VehicleDealer entity = orikaMapper.map(inputDTO, VehicleDealer.class);
+        entity = repository.save(entity);
+        saveEntityWasSuccessful(entity);
+        return orikaMapper.map(entity, VehicleDealerOutputDTO.class);
+    }
+
+    private void saveEntityWasSuccessful(VehicleDealer entity) {
+        try {
+            log.info("VehicleDealerRepository/save(" + objectMapper.writeValueAsString(entity) + ") was successful");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
     }
 
