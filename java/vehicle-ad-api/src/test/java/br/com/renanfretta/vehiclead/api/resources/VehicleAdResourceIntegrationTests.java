273ba1f5da7331a02f0e2e5a251702d1047b3ab3
frettarenan <frettarenan@gmail.com>
2021-06-07 16:21:29 -0300

    class: VehicleAdResourceIntegrationTests > Integration tests completed

---
+++ java/vehicle-ad-api/src/test/java/br/com/renanfretta/vehiclead/api/resources/VehicleAdResourceIntegrationTests.java
@@ -0,0 +1,13 @@
+package br.com.renanfretta.vehiclead.api.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleAdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- java/vehicle-ad-api/src/test/java/br/com/renanfretta/vehiclead/api/resources/VehicleDealerResourceIntegrationTests.java
+++ java/vehicle-ad-api/src/test/java/br/com/renanfretta/vehiclead/api/resources/VehicleDealerResourceIntegrationTests.java
@@ -1,10 +1,10 @@
-package br.com.renanfretta.vehiclead.api;
+package br.com.renanfretta.vehiclead.api.resources;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VehicleAdApiApplicationTests {
+class VehicleAdResourceIntegrationTests {
 
     @Test
     void contextLoads() {
