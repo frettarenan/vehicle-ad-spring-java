ea89fdaca5dec30c634b804de0a1c670d870d1d3
frettarenan <frettarenan@gmail.com>
2021-06-07 18:38:12 -0300

    documentations added

--- java/vehicle-ad-api/src/main/java/br/com/renanfretta/vehiclead/api/resources/VehicleDealerResource.java
+++ java/vehicle-ad-api/src/main/java/br/com/renanfretta/vehiclead/api/resources/VehicleDealerResource.java
@@ -6,6 +6,11 @@ import br.com.renanfretta.vehiclead.api.dtos.vehicledealer.output.VehicleDealerO
 import br.com.renanfretta.vehiclead.api.exceptions.entity.ResourceNotFoundException;
 import br.com.renanfretta.vehiclead.api.services.VehicleDealerService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -25,6 +30,10 @@ public class VehicleDealerResource {
 
     @GetMapping
     @Operation(summary = "List all")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = VehicleDealerOutputDTO.class)))}),
+            @ApiResponse(responseCode = "204", description = "No content", content = @Content)
+    })
     public ResponseEntity<List<VehicleDealerOutputDTO>> findAll() {
         log.info("VehicleDealerResource/findAll was called");
         List<VehicleDealerOutputDTO> list = service.findAll();
@@ -38,6 +47,10 @@ public class VehicleDealerResource {
 
     @GetMapping(value = "/{id}")
     @Operation(summary = "Find by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VehicleDealerOutputDTO.class))}),
+            @ApiResponse(responseCode = "204", description = "No content", content = @Content)
+    })
     public ResponseEntity<VehicleDealerOutputDTO> findById(@PathVariable Long id) throws ResourceNotFoundException {
         log.info("VehicleDealerResource/findById(" + id + ") was called");
         VehicleDealerOutputDTO dto = service.findById(id);
@@ -47,6 +60,9 @@ public class VehicleDealerResource {
 
     @PostMapping
     @Operation(summary = "Save new")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VehicleDealerOutputDTO.class))})
+    })
     public ResponseEntity<VehicleDealerOutputDTO> save(@RequestBody VehicleDealerInputDTO inputDTO) {
         log.info("VehicleDealerResource/save( " + objectMapper.writeValueAsStringNoException(inputDTO) + ") was called");
         VehicleDealerOutputDTO outputDTO = service.save(inputDTO);
@@ -55,6 +71,10 @@ public class VehicleDealerResource {
 
     @PutMapping(value = "/{id}")
     @Operation(summary = "Update all")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VehicleDealerOutputDTO.class))}),
+            @ApiResponse(responseCode = "204", description = "No content", content = @Content)
+    })
     public ResponseEntity<VehicleDealerOutputDTO> updateAll(@PathVariable Long id, @RequestBody VehicleDealerInputDTO inputDTO) throws ResourceNotFoundException {
         log.info("VehicleDealerResource/updateAll(id: " + id + " obj: " + objectMapper.writeValueAsStringNoException(inputDTO) + ") was called");
         VehicleDealerOutputDTO outputDTO = service.updateAll(id, inputDTO);
@@ -63,6 +83,10 @@ public class VehicleDealerResource {
 
     @PatchMapping(value = "/{id}")
     @Operation(summary = "Partial update")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VehicleDealerOutputDTO.class))}),
+            @ApiResponse(responseCode = "204", description = "No content", content = @Content)
+    })
     public ResponseEntity<VehicleDealerOutputDTO> partialUpdate(@PathVariable Long id, @RequestBody VehicleDealerInputDTO inputDTO) throws ResourceNotFoundException {
         log.info("VehicleDealerResource/partialUpdate(id: " + id + " obj: " + objectMapper.writeValueAsStringNoException(inputDTO) + ") was called");
         VehicleDealerOutputDTO outputDTO = service.partialUpdate(id, inputDTO);
@@ -71,6 +95,10 @@ public class VehicleDealerResource {
 
     @DeleteMapping(value = "/{id}")
     @Operation(summary = "Delete by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = VehicleDealerOutputDTO.class))}),
+            @ApiResponse(responseCode = "204", description = "No content", content = @Content)
+    })
     public ResponseEntity<VehicleDealerOutputDTO> deleteById(@PathVariable Long id) throws ResourceNotFoundException {
         log.info("VehicleDealerResource/deleteById(" + id + ") was called");
         VehicleDealerOutputDTO outputDTO = service.deleteById(id);
